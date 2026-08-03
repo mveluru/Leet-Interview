9d8c1c511e201dd305f43537772658235ee2e674
mveluru <mveluru@gmail.com>
2026-08-03 01:58:21 -0500

    Leet

---
+++ src/leetcode75/easy/isPangram.java
@@ -0,0 +1,17 @@
+package leetcode75.easy;
+
+public class isPangram {
+    public static String isPangrams(String sentence) {
+        if (sentence == null || sentence.length() < 26) {
+            return "not pangram";
+        }
+        long uniqueLetters = sentence.toLowerCase().chars().filter(Character::isLetter).distinct().count();
+        return uniqueLetters == 26 ? "pangram" : "not pangram";
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPangrams("We promptly Judged antique ivory Buckles for the next prize"));
+        System.out.println(isPangrams("We promptly judged antique ivory buckles for the prize"));
+    }
+}
