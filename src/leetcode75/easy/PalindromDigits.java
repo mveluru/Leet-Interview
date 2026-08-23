9849d56d6205fdd613fea22ab70a27f8c473e8f3
mveluru <mveluru@gmail.com>
2026-08-23 14:06:53 -0500

    Leet

--- src/leetcode75/easy/PalindromDigits.java
+++ src/leetcode75/easy/PalindromDigits.java
@@ -6,14 +6,40 @@ public class PalindromDigits {
         int orinum = num;
         int reversed = 0;
 
-        while (palindrom >0) {
+        while (palindrom > 0) {
             int digit = palindrom % 10;
-            reversed = reversed*10 + digit;
+            reversed = reversed * 10 + digit;
             palindrom = palindrom / 10;
 
         }
         return reversed == orinum;
     }
+
+    public static void negativePlaindrom(int num) {
+        int sign = 0;
+        int palinNum = 0;
+        int reverseNum = 0;
+
+        if (num < 0) {
+            sign = -1;
+            palinNum = Math.abs(num);
+        }
+        while (palinNum > 0) {
+            reverseNum = reverseNum * 10 + palinNum % 10;
+            palinNum = palinNum / 10;
+        }
+        if (reverseNum == Math.abs(num)) {
+            if (sign == -1) {
+                reverseNum = reverseNum * sign;
+            }
+            System.out.print("Palindrom  " + reverseNum);
+        } else {
+            System.out.print("Not Palindrom " + num);
+        }
+
+
+    }
+
     public static void main(String[] args) {
         System.out.println(isPalindrom(131));
     }
