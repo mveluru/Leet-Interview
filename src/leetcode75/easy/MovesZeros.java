254533b2ee832c08fd7b5247e200de68623b2456
mveluru <mveluru@gmail.com>
2026-08-23 15:35:55 -0500

    Leet

--- src/leetcode75/easy/MovesZeros.java
+++ src/leetcode75/easy/MovesZeros.java
@@ -17,6 +17,18 @@ public class MovesZeros {
         }
         System.out.println(Arrays.toString(leftadjustArray));
     }
+    public static void moveZerosToEnd(int[] nums) {
+        int[] numArray = new int[nums.length];
+        int index=0;
+        for(int value:nums) {
+            if(value!=0) {
+                numArray[index++]=value;
+            }
+        }
+        System.arraycopy(numArray, 0, nums, 0, nums.length);
+        System.out.print(Arrays.toString(nums));
+
+    }
 
     public static void main(String[] args) {
         int[] nums1 = new int[]{0, 1, 0, 3, 12};
