1b7038250e452ac3bff4f10e2107f994135254f8
mveluru <mveluru@gmail.com>
2026-09-03 00:03:58 -0500

    Leet

--- src/leetcode75/medium/MissingNumberInSequence.java
+++ src/leetcode75/medium/MissingNumberInSequence.java
@@ -1,7 +1,8 @@
 package leetcode75.medium;
 
-import java.util.Arrays;
-import java.util.OptionalInt;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class MissingNumberInSequence {
 	public static void MissingNumber(int[] intArray) {
@@ -15,13 +16,22 @@ public class MissingNumberInSequence {
 		}
 	}
 
+	public static void findMissingNumbersInSequence() {
+		//int[] intarray = {2,3,3,3,4,4,5,7,8,9,1};
+		int[] intarray = {2,3,3,3,4,4,7,8,9,1};
+		// removeDuplication
+		Set<Integer> set = Arrays.stream(intarray).boxed().sorted().collect(Collectors.toCollection(LinkedHashSet::new));
+		List<Integer> list = IntStream.rangeClosed(1, 9)
+				.filter(i -> !set.contains(i)).boxed().toList();
+		System.out.println("\n Missing numbers[s]: "+list);
+	}
 
 
 	public static void main(String[] main) {
 		int[] intArray = { 1, 2, 3, 4, 5, 6, 7, 9 };
 		System.out.println(Arrays.toString(intArray));
 		MissingNumber(intArray);
-
+		findMissingNumbersInSequence();
 
 	}
 
