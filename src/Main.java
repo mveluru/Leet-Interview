0db05caf1a5dd05d62b5b8e1ff68fd4bfa2f7aa1
mveluru <mveluru@gmail.com>
2026-09-03 11:20:08 -0500

    Leet

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,23 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.NumberFormat;
+import java.time.Duration;
 import java.util.*;
+import java.util.function.DoublePredicate;
+import java.util.function.Predicate;
+import java.util.stream.DoubleStream;
 import java.util.stream.Stream;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        List<String> original =
-                new ArrayList<>(List.of("A", "B"));
 
-        List<String> view =
-                Collections.unmodifiableList(original);
 
-        original.add("C");
 
-        System.out.println(view);
-        original.add("D");
-        System.out.println(view);
-        original.remove("D");
-        System.out.println(view);
-        System.out.println(original);
+    public static void main(String[] args) throws InterruptedException {
+       List<String> chars = Stream.generate( () -> "abc" ).limit(10).toList();
+        System.out.println(chars);
 
     }
 }
