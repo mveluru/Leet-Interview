1d20d12477bfeda3d71229569627f1f8b9eba730
mveluru <mveluru@gmail.com>
2026-09-08 14:02:18 -0500

    Leet

--- src/leetcode75/easy/SumTwo9.java
+++ src/leetcode75/easy/SumTwo9.java
@@ -1,10 +1,12 @@
 package leetcode75.easy;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class SumTwo9 {
-    public static int[] SumTwo9(int[] nums, int target) {
+    public static int[] sumTwo9(int[] nums, int target) {
 
         Map<Integer, Integer> map = new HashMap<>();
 
@@ -19,12 +21,29 @@ public class SumTwo9 {
         return new int[] {};
     }
 
+    public static void SumOfTwo_9(String[] args) {
+        int [] nums = {2, 7, 11, 15, 3, 6};
+        Set<Integer> seen = new HashSet<>();
+        int target =9;
+        for (int num : nums) {
+            int complement = target - num;
+
+            if (seen.contains(complement)) {
+                System.out.println(complement + "," + num);
+                break;
+            }
+
+            seen.add(num);
+        }
+
+    }
+
     public static void main(String[] args) {
 
         int[] nums = { 1, 11, 8, 15 };
         int targetSum = 9;
 
-        int[] indexes = SumTwo9(nums, targetSum);
+        int[] indexes = sumTwo9(nums, targetSum);
         System.out.printf("%4d,%4d", indexes[0], indexes[1]);
 
     }
