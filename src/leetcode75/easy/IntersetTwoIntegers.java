71e08ba4b5e722c98e49c2a59d71f0b7c63f827c
mveluru <mveluru@gmail.com>
2026-09-13 10:50:07 -0500

    Leet

--- src/leetcode75/easy/IntersetTwoIntegers.java
+++ src/leetcode75/easy/IntersetTwoIntegers.java
@@ -1,15 +1,11 @@
 package leetcode75.easy;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class IntersetTwoIntegers {
+    public static void IntersetTwoIntegers(int[] nums1, int[] nums2) {
 
-
-    public static void main(String[] arg) {
-        int[] nums1 = {1, 2, 2, 3, 4, 5};
-        int[] nums2 = {2, 2, 4, 6};
 
         Set<Integer> set1 = new HashSet<>();
         Set<Integer> result = new LinkedHashSet<>();
@@ -25,5 +21,19 @@ public class IntersetTwoIntegers {
         }
 
         System.out.println(result);
+    }
+
+    public static void IntersetTwoIntegers2(int[] nums1, int[] nums2) {
+        Set<Integer> nums2Set = Arrays.stream(nums2).boxed().collect(Collectors.toSet());
+        List<Integer> results = Arrays.stream(nums1).boxed().distinct().filter(nums2Set::contains).toList();
+        System.out.println(results);
+    }
+
+    public static void main(String[] arg) {
+        int[] nums1 = {1, 2, 2, 3, 4, 5};
+        int[] nums2 = {2, 2, 4, 6};
+        IntersetTwoIntegers(nums1, nums2);
+        IntersetTwoIntegers2(nums1, nums2);
+
     }
 }
